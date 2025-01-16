a8b060f50499a555c8a9fbb87462d417a5f39489
Faiz <faiz199011@gmail.com>
2025-01-16 10:55:09 +0430

    Add: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { FooterCentered } from "../components/footer/FooterCentered";
 
 export default function Home() {
   return (
     <>
-
+      <FooterCentered/>
     </>
   );
 }
