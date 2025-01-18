8e38c3328b51c48e1ef176c2ef332ed85c2908a5
Faiz <faiz199011@gmail.com>
2025-01-18 10:56:33 +0430

    Add: project page

---
+++ src/app/projects/Page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Footer } from "@/components/footer/Footer";
+import { Box } from '@mantine/core';
+import CustomDivider from "../../components/others/CustomDivider";
+
+export default function Projects() {
+  return (
+    <>
+      <Box pb={50} />
+
+      <CustomDivider/>
+
+      <Footer/>
+
+      <Box pb={50} />
+    </>
+  );
+}
