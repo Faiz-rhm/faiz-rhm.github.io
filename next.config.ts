a47bc2d36db400f44afc09c6150c408b3bab06f0
Faiz Rhm <faiz199011@gmail.com>
2025-01-28 15:25:43 +0430

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Enables static export
   reactStrictMode: true,
   images: {
     unoptimized: true
